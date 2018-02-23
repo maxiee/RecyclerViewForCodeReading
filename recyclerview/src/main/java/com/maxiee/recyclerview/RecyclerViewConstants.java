4ef48cfe0dc7a7fda71e9fe0e4587851aaaa4ed8
WangRui <maxieewong@gmail.com>
2018-02-23 16:19:19 +0800

    ViewHolder

---
+++ recyclerview/src/main/java/com/maxiee/recyclerview/RecyclerViewConstants.java
@@ -0,0 +1,7 @@
+package com.maxiee.recyclerview;
+
+public class RecyclerViewConstants {
+    public static final int NO_POSITION = -1;
+    public static final long NO_ID = -1;
+    public static final int INVALID_TYPE = -1;
+}
